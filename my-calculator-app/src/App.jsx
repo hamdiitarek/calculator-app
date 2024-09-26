78699bf40df3ef9dd1c67e9cb1dfc01e93db0ac7
hamdı <hamdi@gmail.com>
2024-09-26 22:26:32 +0300

    first commit

---
+++ my-calculator-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./Calculator";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="calculator-container">
+        <Calculator />
+      </div>
+    </div>
+  );
+}
+
+export default App;
